3741a8d723c5e2497f4d74350b94e7d787903bb6
Vimal <arvimal@yahoo.in>
2016-05-27 13:58:14 +0530

    * Renamed `instance_methods.py` to `17-instance_methods.py`.
    * Added notes in `17-instance_methods.py`.
    
    * Updated 14-multiple-inheritance-1.py, 15-multiple-inheritance-2.py, 16-multiple-inheritance-3.py
    
    * Updated README.md

---
+++ 17-instance_methods.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+# 17-instance_methods.py
+
+# Instance methods are the normal way of accessing methods, seen in all
+# classes till now. ie.. by instantiating instances from a class, and
+# access the methods within the class. The usage of `self` is very
+# important in instance methods due to `self` being a hook/handle to the
+# instance itself, or the instance itself.
+
+# We look into a previous example, once more, to understand `Instance methods`.
+
+# We have an __init__() constructor, and three methods within the
+# `InstanceCounter` class.
+
+# Three instances a, b, and c are created from the class `InstanceCounter`.
+
+# Since the methods defined in the class are accessed through the
+# instances 'a', 'b', and 'c', these methods are called 'Instance
+# methods'.
+
+# Since the instance is bound to the methods defined in the class by the
+# keyword `self`, we also call `Instance methods` as 'Bound methods'.
+
+
+class InstanceCounter(object):
+    count = 0
+
+    def __init__(self, val):
+        self.val = val
+        InstanceCounter.count += 1
+
+    def set_val(self, newval):
+        self.val = newval
+
+    def get_val(self):
+        return self.val
+
+    def get_count(self):
+        return InstanceCounter.count
+
+a = InstanceCounter(5)
+b = InstanceCounter(10)
+c = InstanceCounter(15)
+
+for obj in (a, b, c):
+    print("Value of object: %s" % (obj.get_val))
+    print("Count : %s " % (obj.get_count))
