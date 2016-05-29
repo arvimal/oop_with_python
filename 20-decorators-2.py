b364423f83154fc9ef2a25c875759eb57a10777d
Vimal <arvimal@yahoo.in>
2016-05-29 17:19:01 +0530

    * Added notes in 20-decorators-2.py

--- 20-decorators-2.py
+++ 20-decorators-2.py
@@ -4,6 +4,11 @@
 # By Jillian Munson - PyGotham 2014.
 # https://www.youtube.com/watch?v=yW0cK3IxlHc
 
+# 20-decorators-2.py
+# An updated version of 19-decorators-1.py
+
+# This code snippet takes the previous example, and add a bit more information
+# to the output.
 
 import datetime
 
@@ -22,3 +27,9 @@ def decorated():
 
 if __name__ == "__main__":
     decorated()
+
+# This will print: (NOTE: The time will change of course :P)
+# # python 20-decorators-2.py
+# 2016-05-29 11:46:07.444330
+# This happened!
+# 2016-05-29 11:46:07.444367
