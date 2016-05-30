eada33c062003f07abc0c6755369d77ea2db6c30
Vimal <arvimal@yahoo.in>
2016-05-30 23:57:56 +0530

    * Added 25-decorators-7.py
    * Added 26-class-decorators.py
    
    * Added 27-classmethod-1.py

---
+++ 25-decorators-7.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+# 25-decorators-7.py
+
+# Reference https://www.youtube.com/watch?v=Slf1b3yUocc
+
+# We have two functions, one which adds two numbers, 
+# and another which subtracts two numbers.
+
+# We apply the decorator @double which takes in the
+# functions that is called with the decorator, and doubles
+# the output of the respective function.
+
+
+def double(my_func):
+    def inner_func(a, b):
+        return 2 * my_func(a, b)
+    return inner_func
+
+
+@double
+def adder(a, b):
+    return a + b
+
+
+@double
+def subtractor(a, b):
+    return a - b
+
+
+print(adder(10, 20))
+print(subtractor(6, 1))
---
+++ 26-class-decorators.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+# 26-class-decorators.py
+
+# Reference : https://www.youtube.com/watch?v=Slf1b3yUocc
+# Talk by Mike Burns
+
+# Till the previous examples, we saw function decorators.
+# But decorators can be applied to Classes as well.
+# This example deals with class decorators.
+
+# NOTE: If you need to decorate a Class, you'll need the
+# decorator function to return a Class.
+
+# NOTE: If you need to decorate a Function, you'll need
+# the decorator function to return a function.
+
+
+def honirific(cls):
+    class HonirificCls(cls):
+        def full_name(self):
+            return "Dr. " + super(HonirificCls, self).full_name
+    return HonirificCls
+
+
+@honirific
+class Name(object):
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def full_name(self):
+        return " ".join([self.first_name, self.last_name])
+
+result = Name("Vimal", "A.R").full_name()
+print("Full name: {0}".format(result))
+
+
+# This needs further check. Erroring out.
