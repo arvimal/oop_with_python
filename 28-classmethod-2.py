dd9c5b348370e2fe01dce8988ae828b1bb5f92a1
Vimal <arvimal@yahoo.in>
2016-06-12 23:42:41 +0530

    * Renamed 25-class-methods.py -> 27-classmethod-1.py
    * Renamed 27-classmethod-1.py -> 28-classmethod-2.py
    
    * Updated 29-staticmethod-1.py
    
    * Renamed 30-magicmethods-1.py -> 31-magicmethods-1.py
    
    * Renamed 31-magicmethods-2.py -> 32-magicmethods-2.py
    
    * Renamed 32-attribute-encapsulation-1.py -> 33-attribute-encapsulation-1.py
    
    * Deleted static_methods.py

---
+++ 28-classmethod-2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+# 27-classmethod-1.py
+
+# Classmethods are decorators which are inbuilt in Python.
+# We decorate a function as a classemethod using the decorator
+# @classmethod.
+
+# Class methods are used for functions which need not be 
+# called via an instance. Certain use cases may be:
+
+# a) Creating instances take resources, hence the methods/functions
+# which need necessarily 
--- 29-staticmethod-1.py
+++ 29-staticmethod-1.py
@@ -1,5 +1,17 @@
 #!/usr/bin/env python3
 
+# 29-staticmethod-1.py
+
+# Refer
+# https://arvimal.wordpress.com/2016/06/12/instance-class-static-methods-object-oriented-programming/
+
+# Static methods are functions/methods which doesn’t need a binding to a class or an instance.
+# Static methods, as well as Class methods, don’t require an instance to be called.
+# Static methods doesn’t need  self or cls as the first argument since it’s not bound to an instance or class.
+# Static methods are normal functions, but within a class.
+# Static methods are defined with the keyword @staticmethod above the function/method.
+# Static methods are usually used to work on Class Attributes.
+
 
 class InstanceCounter(object):
     count = 0
