4945b4213b20be027095c3ea4c4fe8d3318352fa
Vimal <arvimal@yahoo.in>
2016-06-14 11:20:04 +0530

    * Updated 34-abstractclasses-1.py

--- 34-abstractclasses-1.py
+++ 34-abstractclasses-1.py
@@ -2,37 +2,58 @@
 
 # 34-abstractclasses-1.py
 
-# This code snippet talks about Abstract Base Classes (abc)
+# This code snippet talks about Abstract Base Classes (abc).
+
+# The `abc` module provides features to create
+# Abstract Base Classes.
+
+# To create an Abstract Base Class, set the `__metaclass__` magic method
+# to `abc.ABCMeta`. This will mark the respective class as an Abstract
+# Base Class.
+
+# Now, in order to specify the methods which are to be enforced on the
+# child classes, ie.. to create Abstract Methods, we use the decorator
+# @abc.abstractmethod on the methods we need.
+
+# The child class which inherits from an Abstract Base Class can implement
+# methods of their own, but *should always* implement the methods defined in
+# the parent ABC Class.
 
 import abc
 
-# Abstact Base Class defined below
 
-class GetterSetter(object):
+class My_ABC_Class(object):
     __metaclass__ = abc.ABCMeta
 
     @abc.abstractmethod
     def set_val(self, val):
-        """Set a value in the instance"""
         return
 
     @abc.abstractmethod
     def get_val(self):
-        """Get a return value from the instance"""
         return
 
 # Abstract Base Class defined above ^^^
 
-# Custom class inheriting from the above Abstract Base Class
+# Custom class inheriting from the above Abstract Base Class, below
 
-class MyClass(GetterSetter):
 
-    def get_val(self, input):
+class MyClass(My_ABC_Class):
+
+    def set_val(self, input):
         self.val = input
 
     def get_val(self):
+        print("\nCalling the get_val() method")
+        print("I'm part of the Abstract Methods defined in My_ABC_Class()")
         return self.val
 
+    def hello(self):
+        print("\nCalling the hello() method")
+        print("I'm *not* part of the Abstract Methods defined in My_ABC_Class()")
 
 my_class = MyClass()
-print(my_class)
+
+my_class.set_val(10)
+print(my_class.get_val())
+my_class.hello()
